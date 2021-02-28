9355ffdff9856b79a7d376174ffb30095fda47aa
Mmontsheng Maoto <mmontshengmaoto@gmail.com>
2021-02-28 19:05:26 +0200

    proper nginx error handling

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid">
     <div class="text-center justify-content-center align-self-center">
-          <img width="400" height="400" class="img-fluid" src="@/assets/404.svg" alt="" />
+          <img width="300" height="300" class="img-fluid" src="@/assets/404.svg" alt="" />
       <h4 class="mt-5">This page is outside of the Universe</h4>
     <div class="d-flex justify-content-center mt-2">
       <button
